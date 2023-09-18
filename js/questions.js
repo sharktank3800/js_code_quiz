af2d2bbc1c3c9e82faa946f55a0b9cee00ed9dc9
[Ayman Shiham] <sharktank38000@gmail.com>
2023-09-17 22:20:10 -0400

    added questions choices correctanswer for the quiz in questions.js

---
+++ js/questions.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+        Text:"What does the 'typeof' operator return for a variable holding a string?",
+        choices: ["A) String", "B) Number", "C) Boolean", "D) Undefined"],
+        correctAnswer:"A) String"
+    },
+
+    
+    {
+        Text:"Which keyword is used to declare a variable with block scope in JavaScript?",
+        choices: ["A) var", "B) let", "C) const", "D) variable"],
+        correctAnswer:"B) let"
+    },
+
+    
+    {
+        Text:" What does the === operator in JavaScript compare?",
+        choices: ["A) Values", "B) Types and values", "C) Types", "D) Memory addresses"],
+        correctAnswer:"B) Types and values"
+    },
+
+    
+    {
+        Text:"Which of the following is not a valid way to comment in JavaScript?",
+        choices: ["A) // This is a comment", "B) /* This is a comment */", "C) ''' This is a comment '''", "D) # This is a comment"],
+        correctAnswer:"D) # This is a comment"
+    },
+
+    
+    {
+        Text:"What method is used to add an element to the end of an array in JavaScript?",
+        choices: ["A) push()", "B) append()", "C) addToEnd()", "D) insertEnd()"],
+        correctAnswer:"A) push()"
+    }
+];
